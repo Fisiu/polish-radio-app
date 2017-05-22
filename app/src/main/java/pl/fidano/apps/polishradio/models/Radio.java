7dc1d4c57b8d342f8f8c997082045bd872c4443e
Mariusz Fik <mariusz@fidano.pl>
2017-05-22 23:41:04 +0200

    Add radio model.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ app/src/main/java/pl/fidano/apps/polishradio/models/Radio.java
@@ -0,0 +1,47 @@
+package pl.fidano.apps.polishradio.models;
+
+public class Radio {
+    private String mLogoUrl;
+    private String mName;
+    private String mUrl;
+    private String mStreamUrl;
+
+    public Radio(String mLogoUrl, String mName, String mUrl, String mStreamUrl) {
+        this.mLogoUrl = mLogoUrl;
+        this.mName = mName;
+        this.mUrl = mUrl;
+        this.mStreamUrl = mStreamUrl;
+    }
+
+    public String getLogoUrl() {
+        return mLogoUrl;
+    }
+
+    public void setLogoUrl(String mLogoUrl) {
+        this.mLogoUrl = mLogoUrl;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getStreamUrl() {
+        return mStreamUrl;
+    }
+
+    public void setStreamUrl(String mStreamUrl) {
+        this.mStreamUrl = mStreamUrl;
+    }
+}
