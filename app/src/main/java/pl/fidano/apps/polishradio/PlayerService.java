c9c369635e7b0ae46d4121086da9e55630158562
Mariusz Fik <mariusz@fidano.pl>
2017-06-13 23:49:46 +0200

    Service refactoring. Implement notifications.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- app/src/main/java/pl/fidano/apps/polishradio/PlayerService.java
+++ app/src/main/java/pl/fidano/apps/polishradio/PlayerService.java
@@ -11,19 +11,21 @@ import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
+
+import pl.fidano.apps.polishradio.models.Radio;
 
 public class PlayerService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
     private static final String TAG = "PlayerService";
 
-    private static final int ONGOING_NOTIFICATION_ID = 1;
+    private static final int RADIO_PLAYER_NOTIFICATION_ID = 1;
 
     private static final String ACTION_PLAY = "pl.fidano.apps.polishradio.action.PLAY";
     private static final String ACTION_STOP = "pl.fidano.apps.polishradio.action.STOP";
 
     private WifiManager.WifiLock mWifiLock;
-    private PowerManager.WakeLock wakeLock;
 
     private MediaPlayer mPlayer;
 
@@ -37,80 +39,43 @@ public class PlayerService extends Service implements MediaPlayer.OnPreparedList
     public void onCreate() {
         Log.d(TAG, "running onCreate...");
 
+        mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        mPlayer.setOnPreparedListener(this);
+        mPlayer.setOnErrorListener(this);
 
         mWifiLock = ((WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE))
                 .createWifiLock(WifiManager.WIFI_MODE_FULL, "PlayerWiFiLockTag");
 
-//        wakeLock = ((PowerManager) getApplicationContext().getSystemService(POWER_SERVICE))
-//                .newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "PlayerWakeLockTag");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//        super.onStartCommand(intent, flags, startId);
+        super.onStartCommand(intent, flags, startId);
 
         Log.d(TAG, "running onStartCommand...");
 
-        if(intent.getAction().equals(ACTION_PLAY)) {
-            mPlayer = new MediaPlayer();
+        Radio radio = (Radio) intent.getSerializableExtra("radio");
+
+        if (intent.getAction().equals(ACTION_PLAY)) {
 
             mPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-//            playRadio();
-
             mWifiLock.acquire();
-//            wakeLock.acquire();
-
-            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
             try {
-                mPlayer.setDataSource(intent.getStringExtra("url"));
-                mPlayer.setOnPreparedListener(this);
-                mPlayer.setOnErrorListener(this);
-                mPlayer.prepareAsync(); // prepare async to not block main thread
+                mPlayer.reset(); // when switching between stations, first reset player
+                mPlayer.setDataSource(radio.getStreamUrl());
             } catch (IOException e) {
+                Log.e(TAG, "Exception when setting stream data source");
+
                 e.printStackTrace();
             }
+
+            mPlayer.prepareAsync(); // prepare async to not block main thread
+            showNotification(radio);
         }
-        // return super.onStartCommand(intent, flags, startId);
         return START_STICKY;
-    }
-
-    private void playRadio() {
-        Intent notificationIntent = new Intent(this, PolishRadio.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        Notification notification = new Notification.Builder(this)
-                .setContentTitle("Polish Radio")
-                .setContentText("Nazwa stacji")
-                .setSmallIcon(R.drawable.ic_menu_camera)
-                .setContentIntent(pendingIntent)
-                .setTicker("Ticker text")
-                .build();
-
-        startForeground(ONGOING_NOTIFICATION_ID, notification);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "running onDestroy...");
-
-        if (mPlayer != null) {
-            if(mPlayer.isPlaying()) {mPlayer.stop();}
-
-            mPlayer.reset();
-            mPlayer.release();
-            mPlayer = null;
-        }
-
-        if(wakeLock != null && wakeLock.isHeld()) {
-            wakeLock.release();
-        }
-
-        if (mWifiLock != null && mWifiLock.isHeld()) {
-            mWifiLock.release();
-        }
-
     }
 
     @Override
@@ -121,12 +86,67 @@ public class PlayerService extends Service implements MediaPlayer.OnPreparedList
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "running onDestroy...");
+
+        stopForeground(true);
+
+        if (mPlayer != null) {
+            if (mPlayer.isPlaying()) {
+                mPlayer.stop();
+            }
+            mPlayer.release();
+            mPlayer = null;
+        }
+
+        if (mWifiLock != null && mWifiLock.isHeld()) {
+            mWifiLock.release();
+        }
+
+    }
+
+    @Override
     public boolean onError(MediaPlayer mp, int what, int extra) {
         Log.d(TAG, "running onError...");
-        // return false;
         // TODO: Enhance error handling
-        mp.reset();
-        wakeLock.release();
+
+        switch (what) {
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                showToast("Unknown media error");
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                showToast("Media server died");
+                break;
+        }
+
+
+        Log.d(TAG, "extra: " + extra);
+
+        stopForeground(true);
+        mPlayer.reset();
+        if (mWifiLock != null && mWifiLock.isHeld()) {
+            mWifiLock.release();
+        }
         return true; // error was handled
+    }
+
+    private void showNotification(Radio radio) {
+        Intent notificationIntent = new Intent(this, PolishRadio.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle(radio.getName())
+                .setContentText(radio.getUrl())
+                .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+                .setContentIntent(pendingIntent)
+                .setTicker(radio.getName())
+                .build();
+
+        startForeground(RADIO_PLAYER_NOTIFICATION_ID, notification);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
